77e0e79c45a34cb1df147f4e315da0dca3e7f88b
lleoeric <62811623+lleoeric@users.noreply.github.com>
2023-05-13 20:39:48 +0800

    2023.5.13 22：39 提交，初始化项目。

---
+++ src/test/java/cn/leo/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.leo.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
